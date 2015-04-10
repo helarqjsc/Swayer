9d2035f59937896b038afabe129ef70731b70035
Andrey Keske <hello@andreykeske.com>
2015-04-10 18:50:21 +0300

    update

--- gulpfile.js
+++ gulpfile.js
@@ -126,7 +126,7 @@ gulp.task('wiredep', function () {
     .pipe(gulp.dest('app'));
 });
 
-gulp.task('build', ['jshint', 'html', 'images', 'fonts', 'extras', 'samples'], function () {
+gulp.task('build', ['html', 'images', 'fonts', 'extras', 'samples'], function () {
   return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
 });
 
